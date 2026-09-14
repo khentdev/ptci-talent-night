0393ece7f0c1da7e2d9eae784d30cbb0c10f4665
Khentdev <khentplays@gmail.com>
2026-09-14 15:26:32 +0800

    fix: prevent judge access for judge pages

--- ptci_tabulation/src/features/client/clientRoutes.ts
+++ ptci_tabulation/src/features/client/clientRoutes.ts
@@ -1,22 +1,41 @@
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteRecordRaw } from "vue-router";
+import { useAuthStore } from "../auth/store/authStore.ts";
 
 export const clientRoutes: RouteRecordRaw[] = [
-    {
-        path: "home",
-        name: "judge-home",
-        meta: { requiresAuth: true },
-        component: () => import("./Home.vue")
+  {
+    path: "home",
+    name: "judge-home",
+    meta: { requiresAuth: true },
+    component: () => import("./Home.vue"),
+    beforeEnter: () => {
+      const authStore = useAuthStore();
+      if (authStore.getUserMetaData?.role === "admin") {
+        return { name: "home-default" };
+      }
     },
-    {
-        path: "male-candidates/talent",
-        name: "male-candidates-talent",
-        meta: { requiresAuth: true },
-        component: () => import("./male.candidates/Talent.vue")
+  },
+  {
+    path: "male-candidates/talent",
+    name: "male-candidates-talent",
+    meta: { requiresAuth: true },
+    component: () => import("./male.candidates/Talent.vue"),
+    beforeEnter: () => {
+      const authStore = useAuthStore();
+      if (authStore.getUserMetaData?.role === "admin") {
+        return { name: "home-default" };
+      }
     },
-    {
-        path: "female-candidates/talent",
-        name: "female-candidates-talent",
-        meta: { requiresAuth: true },
-        component: () => import("./female.candidates/Talent.vue")
+  },
+  {
+    path: "female-candidates/talent",
+    name: "female-candidates-talent",
+    meta: { requiresAuth: true },
+    component: () => import("./female.candidates/Talent.vue"),
+    beforeEnter: () => {
+      const authStore = useAuthStore();
+      if (authStore.getUserMetaData?.role === "admin") {
+        return { name: "home-default" };
+      }
     },
-]
+  },
+];

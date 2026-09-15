5601dc3487b7593adb9bba47fb025fbe7de420ac
Khentdev <khentplays@gmail.com>
2026-09-16 00:14:49 +0800

    fix: add .length check to trigger empty-state in judge-scores-data props

--- ptci_tabulation/src/features/talent/components/JudgesScoresDataTable.vue
+++ ptci_tabulation/src/features/talent/components/JudgesScoresDataTable.vue
@@ -11,7 +11,7 @@
           type === 'female' ? 'females' : 'males'
         }. Please check your connection and try again.`"
       />
-      <is-empty-state v-else-if="!judgesScoresData" />
+      <is-empty-state v-else-if="!judgesScoresData?.length" />
       <table :class="TABLE_STYLES.TB" v-else>
         <thead :class="TABLE_STYLES.THEADROW">
           <tr>

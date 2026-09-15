e76ff6a5b7ed01be02ba893b4a6c9364621c74db
Khentdev <khentplays@gmail.com>
2026-09-15 11:50:25 +0800

    fix: redirect to Homepage.vue as default view

--- ptci_tabulation/src/app/router/index.ts
+++ ptci_tabulation/src/app/router/index.ts
@@ -1,95 +1,97 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import { useAuthStore } from '../../features/auth/store/authStore';
-import { authRoutes } from '../../features/auth/authRoutes';
-import { homeRoutes, dashboardOverviewRoutes } from '../../features/dashboard/dashboardRoutes';
-import { talentRoutes } from '../../features/talent/talentRoutes';
-import { settingsRoutes } from '../../features/settings/settingsRoutes';
-import { clientRoutes } from '../../features/client/clientRoutes';
+import { useAuthStore } from "../../features/auth/store/authStore";
+import { authRoutes } from "../../features/auth/authRoutes";
+import {
+  homeRoutes,
+  dashboardOverviewRoutes,
+} from "../../features/dashboard/dashboardRoutes";
+import { talentRoutes } from "../../features/talent/talentRoutes";
+import { settingsRoutes } from "../../features/settings/settingsRoutes";
+import { clientRoutes } from "../../features/client/clientRoutes";
 
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: "",
-        name: "root",
-        component: () => import("../../views/initialMount.vue")
+  {
+    path: "",
+    name: "root",
+    component: () => import("../../views/initialMount.vue"),
+  },
+  {
+    path: "/auth",
+    name: "auth",
+    component: () => import("../layouts/authLayout.vue"),
+    redirect: { name: "login" },
+    children: [...authRoutes],
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("../layouts/appLayout.vue"),
+    redirect: { name: "home-default" },
+    children: [...homeRoutes],
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    meta: { requiresAuth: true },
+    redirect: { name: "dashboard-overview" },
+    component: () => import("../layouts/dashboardLayout.vue"),
+    beforeEnter: () => {
+      const authStore = useAuthStore();
+      if (!authStore.isLoggedIn || authStore.getUserMetaData?.role !== "admin")
+        return { name: "home-default" };
     },
-    {
-        path: "/auth",
-        name: "auth",
-        component: () => import("../layouts/authLayout.vue"),
-        redirect: { name: "login" },
-        children: [...authRoutes]
-    },
-    {
-        path: "/home",
-        name: "home",
-        component: () => import("../layouts/appLayout.vue"),
+    children: [
+      ...dashboardOverviewRoutes,
+      ...talentRoutes,
+      ...settingsRoutes,
+      {
+        path: ":pathMatch(.*)*",
+        name: "dashboard-not-found",
         meta: { requiresAuth: true },
-        redirect: { name: "home-default" },
-        children: [...homeRoutes]
-    },
-    {
-        path: "/dashboard",
-        name: "dashboard",
-        meta: { requiresAuth: true },
-        redirect: { name: "dashboard-overview" },
-        component: () => import("../layouts/dashboardLayout.vue"),
-        beforeEnter: () => {
-            const authStore = useAuthStore()
-            if (!authStore.isLoggedIn || authStore.getUserMetaData?.role !== "admin")
-                return { name: "home-default" }
-        },
-        children: [
-            ...dashboardOverviewRoutes,
-            ...talentRoutes,
-            ...settingsRoutes,
-            {
-                path: ":pathMatch(.*)*",
-                name: "dashboard-not-found",
-                meta: { requiresAuth: true },
-                component: () => import("../../features/shared/components/404/AppNotFound.vue")
-            }
-        ]
-    },
-    {
-        path: "/judge",
-        name: "judge",
-        component: () => import("../layouts/ClientLayout.vue"),
-        redirect: { name: "judge-home" },
-        meta: { requiresAuth: true },
-        children: [...clientRoutes]
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        name: "global-not-found",
-        component: () => import("../../views/404/Global404NotFound.vue")
-    }
-
-]
+        component: () =>
+          import("../../features/shared/components/404/AppNotFound.vue"),
+      },
+    ],
+  },
+  {
+    path: "/judge",
+    name: "judge",
+    component: () => import("../layouts/ClientLayout.vue"),
+    redirect: { name: "judge-home" },
+    meta: { requiresAuth: true },
+    children: [...clientRoutes],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "global-not-found",
+    component: () => import("../../views/404/Global404NotFound.vue"),
+  },
+];
 export const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 router.beforeEach(async (to, from, next) => {
-    console.log("Guard entry:", {
-        original: to.fullPath,
-        toName: to.name,
-        toPath: to.path,
-        fromName: from.name,
-    });
+  console.log("Guard entry:", {
+    original: to.fullPath,
+    toName: to.name,
+    toPath: to.path,
+    fromName: from.name,
+  });
 
-    const authStore = useAuthStore()
+  const authStore = useAuthStore();
 
-    const requiresAuth = to.matched.some(r => r.meta.requiresAuth)
-    if (!requiresAuth) return next()
+  const requiresAuth = to.matched.some((r) => r.meta.requiresAuth);
+  if (!requiresAuth) return next();
 
-    if (requiresAuth && !authStore.loadingState.sessionInitialized) {
-        if (authStore.isLoggedIn) return next()
-        const res = await authStore.refreshSession()
-        if (!res.success && res.logout) return next({ name: "login" })
-    }
-    return next()
-})
+  if (requiresAuth && !authStore.loadingState.sessionInitialized) {
+    if (authStore.isLoggedIn) return next();
+    const res = await authStore.refreshSession();
+    if (!res.success && res.logout) return next({ name: "login" });
+  }
+  return next();
+});
--- ptci_tabulation/src/features/dashboard/dashboardRoutes.ts
+++ ptci_tabulation/src/features/dashboard/dashboardRoutes.ts
@@ -1,19 +1,18 @@
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteRecordRaw } from "vue-router";
 
 export const homeRoutes: RouteRecordRaw[] = [
-    {
-        path: "",
-        name: "home-default",
-        meta: { requiresAuth: true },
-        component: () => import("./views/Homepage.vue"),
-    }
-]
+  {
+    path: "",
+    name: "home-default",
+    component: () => import("./views/Homepage.vue"),
+  },
+];
 
 export const dashboardOverviewRoutes: RouteRecordRaw[] = [
-    {
-        path: "overview",
-        name: "dashboard-overview",
-        meta: { requiresAuth: true },
-        component: () => import("./views/Overview.vue"),
-    }
-]
+  {
+    path: "overview",
+    name: "dashboard-overview",
+    meta: { requiresAuth: true },
+    component: () => import("./views/Overview.vue"),
+  },
+];
